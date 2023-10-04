48a3da19465eeeb0d1ee1f76424a5f31975a0b3a
Miyaoww <email@miyaoww.com>
2023-10-04 17:06:06 +0700

    Add test

--- basic-programing/operasi/aritmatika.js
+++ basic-programing/operasi/aritmatika.js
@@ -10,5 +10,14 @@ let pengurangan = angka1-angka2
 let perkalian = angka1*angka2
 let pembagian = angka1/angka2
 let sisaBagi = angka1%angka2
-
 console.log("Hasil pengurangan=",pengurangan, "Hasil perkalian=",perkalian, "Hasil pembagian=",pembagian, "Hasil sisa bagi=",sisaBagi)
+    
+// Increment
+
+let myNumber = 1
+// myNumber++
+myNumber+=5
+
+console.log(myNumber)
+
+
