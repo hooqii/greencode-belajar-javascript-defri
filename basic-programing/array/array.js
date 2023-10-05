7ab815d36087a673bc8a4e38eb4214c741354608
Miyaoww <email@miyaoww.com>
2023-10-05 22:13:41 +0700

    Update Array

--- basic-programing/array/array.js
+++ basic-programing/array/array.js
@@ -1,8 +1,10 @@
 let arr = [2, 3, 4, 6, 4]
-
+let firstName = "Defri"
+let lastName = "Salwan"
+let inisial = firstName[0]+lastName[0]
 let penjumlahan = arr[3]+arr[4]
 
 console.log(penjumlahan)
-
+console.log(inisial)
 //Index hanya ada di dalam tipe data array dan string
 //Index di mulai dari 0
---
+++ basic-programing/array/test.js
@@ -0,0 +1,4 @@
+let tes = ["Ini Tes", 1,2,3,4,5]
+let iniTest = tes[0]
+let number = tes[2]+tes[4]+tes[4]
+console.log(iniTest+" "+number)
